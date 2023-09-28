312f2e77b30690e843f472a3e50fcf2f85e53841
JiovannyVega <erickjiovannygamezvega@hotmail.com>
2023-09-27 22:58:28 -0600

    Correccion de errores en AlgoritmoAditivo

--- Algoritmos_Congruenciales/AlgoritmoAditivo.java
+++ Algoritmos_Congruenciales/AlgoritmoAditivo.java
@@ -9,7 +9,7 @@ public class AlgoritmoAditivo {
     public static void inicio() {
         Scanner leer = new Scanner(System.in);
         Vector<Integer> vector = new Vector<Integer>(0);
-        int m, n;
+        int m, n, N;
         System.out.println("Indica con cuantos numeros iniciará Xi:\t");
         n = leer.nextInt();
         for (int i = 0; i < n; i++) {
@@ -19,12 +19,11 @@ public class AlgoritmoAditivo {
         System.out.println("Ingrese el modulo:\t");
         m = leer.nextInt();
         System.out.println("Indique cuantos numeros ri desea generar:\t");
-        for(int i = 0; i < leer.nextInt(); i++) {
-            vector.add(vector.get(i) + vector.get(i + n - 2));
-            System.out.println("Hola mundoooooo");
-            System.out.print("X" + (i + 1) + "= " + vector.get(i + n - 1));
-            double d = vector.get(i + n + 1);
-            d /= (m - 1);
+        N = leer.nextInt();
+        for(int i = 0; i < N; i++) {
+            vector.add((vector.get(i) + vector.get(i + n - 1)) % m);
+            System.out.println("X" + (i + 1) + "= " + vector.get(i + n - 1));
+            double d = vector.get(i + n) / (m - 1);
             System.out.println("\n\tr" + (i + 1) + "= " + Metodos.format4Dec(d));
         }
         leer.close();
