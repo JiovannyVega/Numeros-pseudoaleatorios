267cbff5db6c156700d27004c94c84c6fe8d741d
JiovannyVega <erickjiovannygamezvega@hotmail.com>
2023-09-27 23:32:05 -0600

    Se cambiaron algunas cosas

--- src/Algoritmos_Congruenciales/AlgoritmoAditivo.java
+++ src/Algoritmos_Congruenciales/AlgoritmoAditivo.java
@@ -9,12 +9,13 @@ public class AlgoritmoAditivo {
     public static void inicio() {
         Scanner leer = new Scanner(System.in);
         Vector<Integer> vector = new Vector<Integer>(0);
-        int m, n, N;
+        int m, n, N, aux = 0;
         System.out.println("Indica con cuantos numeros iniciará Xi:\t");
         n = leer.nextInt();
         for (int i = 0; i < n; i++) {
             System.out.println("Ingresa el valor de X" + i + ":\t");
             vector.add(leer.nextInt());
+            aux = i;
         }
         System.out.println("Ingrese el modulo:\t");
         m = leer.nextInt();
@@ -22,8 +23,10 @@ public class AlgoritmoAditivo {
         N = leer.nextInt();
         for(int i = 0; i < N; i++) {
             vector.add((vector.get(i) + vector.get(i + n - 1)) % m);
-            System.out.println("X" + (i + 1) + "= " + vector.get(i + n - 1));
-            double d = vector.get(i + n) / (m - 1);
+            System.out.println("X" + (aux +i + 1) + "= " + vector.get(i + n - 1));
+            double d = vector.get(i + n - 1);
+            d  /= (m - 1);
+            System.out.println(d);
             System.out.println("\n\tr" + (i + 1) + "= " + Metodos.format4Dec(d));
         }
         leer.close();
